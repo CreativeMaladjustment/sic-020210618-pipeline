1c8e4f8f46be13b3e46262080af3a22547fa586b
FloutEnvy <jd@nonviolence.us>
2021-06-15 15:38:28 -0600

    adding a cloud9 env stack

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from aws_cdk import core
 
 from sic_020210618_pipeline.pipeline_stack import PipelineStack
 from sic_020210618_pipeline.lambda_stack import LambdaStack
+from sic_020210618_pipeline.cloud9_stack import Cloud9Stack
 
 app = core.App()
 
@@ -15,4 +16,5 @@ PipelineStack(app, "PipelineDeployingLambdaStack",
     lambda_code=lambda_stack.lambda_code,
     repo_name=CODECOMMIT_REPO_NAME)
 
+cloud9_stack = Cloud9Stack(app, "sic020210618Cloud9")
 app.synth()
