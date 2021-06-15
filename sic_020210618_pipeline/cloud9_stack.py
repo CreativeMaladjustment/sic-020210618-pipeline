b1add2b146b0bfed64a813f0cba7940d985df762
FloutEnvy <jd@nonviolence.us>
2021-06-15 16:01:10 -0600

    forgot to add in this file, the cloud9 stack

---
+++ sic_020210618_pipeline/cloud9_stack.py
@@ -0,0 +1,14 @@
+from aws_cdk import core, aws_cloud9 as cloud9, aws_ec2 as ec2, aws_codecommit as codecommit
+
+class Cloud9Stack(core.Stack):
+  def __init__(self, app: core.App, id: str, **kwargs):
+    super().__init__(app, id, **kwargs)
+
+    vpc = ec2.Vpc(self, "sic020210618-vpc", max_azs=3)
+    repo_existing = codecommit.Repository.from_repository_name(self, "RepoExisting", "sic-020210618-pipeline")
+    c9env = cloud9.Ec2Environment(self, "sic020210618-ec2", vpc=vpc,
+        cloned_repositories=[
+            cloud9.CloneRepository.from_code_commit(repo_existing, "/src/sic-020210618-pipeline")
+        ])
+
+    core.CfnOutput(self, "URL", value=c9env.ide_url)
