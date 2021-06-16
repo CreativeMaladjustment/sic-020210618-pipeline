00b114d421f3dc330bda9d8a36e26954d4515507
jason.davis <jason.davis@stelligent.com>
2021-06-16 14:49:40 +0000

    adding cloud9 stack

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from aws_cdk.core import Stack, Tags
 
 from sic_020210618_pipeline.pipeline_stack import PipelineStack
 from sic_020210618_pipeline.lambda_stack import LambdaStack
+from sic_020210618_pipeline.cloud9_stack import Cloud9Stack
 
 def tag_stack(stack: Stack, tags: dict):
     for key, value in tags.items():
@@ -20,6 +21,9 @@ app = core.App()
 
 lambda_stack = LambdaStack(app, "sic020210618-Lambda")
 tag_stack(lambda_stack, standard_tags)
+
+cloud9_stack = LambdaStack(app, "sic020210618-cloud9-pipeline")
+tag_stack(cloud9_stack, standard_tags)
 
 pipeline_stack = PipelineStack(app, "sic020210618-Pipeline", lambda_code=lambda_stack.lambda_code, repo_name=CODECOMMIT_REPO_NAME)
 tag_stack(pipeline_stack, standard_tags)
