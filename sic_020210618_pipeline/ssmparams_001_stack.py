497327d5375cc19d744f26e4458a6e267e82807e
jason.davis <jason.davis@stelligent.com>
2021-06-17 22:42:36 +0000

    self not stack

--- sic_020210618_pipeline/ssmparams_001_stack.py
+++ sic_020210618_pipeline/ssmparams_001_stack.py
@@ -4,7 +4,7 @@ class Feature001Stack(core.Stack):
   def __init__(self, app: core.App, id: str, **kwargs):
     super().__init__(app, id, **kwargs)
 
-    param001 = ssm.StringParameter(stack, "sic_020210618_pipeline_feature001_ssm001",
+    param001 = ssm.StringParameter(self, "sic_020210618_pipeline_feature001_ssm001",
         description="feature 001 requires this ssm param to be set",
         parameter_name="sic_020210618_pipeline/feature001_001",
         string_value="rabbit"
