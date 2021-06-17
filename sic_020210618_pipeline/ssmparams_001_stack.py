76d50a9b1cdb89f804c441f1d59eb27f171636b1
jason.davis <jason.davis@stelligent.com>
2021-06-17 18:41:42 +0000

    added param for feature001

--- sic_020210618_pipeline/ssmparams_001_stack.py
+++ sic_020210618_pipeline/ssmparams_001_stack.py
@@ -1,1 +1,11 @@
-foo
+from aws_cdk import core, aws_ssm as ssm
+
+class Feature001Stack(core.Stack):
+  def __init__(self, app: core.App, id: str, **kwargs):
+    super().__init__(app, id, **kwargs)
+
+    param = ssm.StringParameter(stack, "sic_020210618_pipeline_feature001_ssm001",
+        description="feature 001 requires this ssm param to be set",
+        parameter_name="sic_020210618_pipeline/feature001_001"
+        string_value="rabbit"
+    )
