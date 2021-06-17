bb0474238b91cd497d4a98ebd1a6d59a0b5ee6e7
jason.davis <jason.davis@stelligent.com>
2021-06-17 22:39:04 +0000

    uncomment param 001

--- sic_020210618_pipeline/ssmparams_001_stack.py
+++ sic_020210618_pipeline/ssmparams_001_stack.py
@@ -4,8 +4,14 @@ class Feature001Stack(core.Stack):
   def __init__(self, app: core.App, id: str, **kwargs):
     super().__init__(app, id, **kwargs)
 
-    # param = ssm.StringParameter(stack, "sic_020210618_pipeline_feature001_ssm001",
-    #     description="feature 001 requires this ssm param to be set",
-    #     parameter_name="sic_020210618_pipeline/feature001_001",
-    #     string_value="rabbit"
+    param001 = ssm.StringParameter(stack, "sic_020210618_pipeline_feature001_ssm001",
+        description="feature 001 requires this ssm param to be set",
+        parameter_name="sic_020210618_pipeline/feature001_001",
+        string_value="rabbit"
+    )
+    
+    # param002 = ssm.StringParameter(stack, "sic_020210618_pipeline_feature001_ssm002",
+    #     description="feature 002 requires this ssm param to be set",
+    #     parameter_name="sic_020210618_pipeline/feature001_002",
+    #     string_value="rabbits many many rabbits"
     # )
